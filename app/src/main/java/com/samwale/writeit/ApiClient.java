01f610bc0febb4d37138b76c0fbe70e067bcadd6
samadeoye <adeoyesamuel14@gmail.com>
2025-06-06 23:48:33 +0400

    Changed API base URL

--- app/src/main/java/com/samwale/writeit/ApiClient.java
+++ app/src/main/java/com/samwale/writeit/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://api.kpakpandohub.com/"; //"http://10.0.2.2:8080/";
+    private static final String BASE_URL = "https://writeit.bestwesternplusikeja.com/"; //"http://10.0.2.2:8080/";
     private static Retrofit retrofit = null;
 
     public static JournalApi getJournalApi() {
